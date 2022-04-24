34bbea6add367191f67552ba064c161b411a5dd3
Julian Luna <julian.lunaarecha@web.com>
2022-04-24 20:10:47 -0300

    Fix hello

--- src/main/java/com/flashhammer/backend/controller/RestEndpoint.java
+++ src/main/java/com/flashhammer/backend/controller/RestEndpoint.java
@@ -2,17 +2,22 @@ package com.flashhammer.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/endpoint")
 public class RestEndpoint {
 
-    @GetMapping(path = "/getUrls")
-    public String getUrls() {
-
+    @GetMapping(path = "/hello")
+    public String hello() {
         String response = "Hello man!";
         return response;
+    }
+
+    @GetMapping(path = "/getUrls")
+    public @ResponseBody Object getUrls() {
+        return null;
     }
     
 }
