d22d3699f2e38e9d3df8f0edcf08fb938d9c03eb
Julian Luna <julian.lunaarecha@web.com>
2022-04-24 21:21:12 -0300

    Jdbc config

--- src/main/java/com/flashhammer/backend/controller/RestEndpoint.java
+++ src/main/java/com/flashhammer/backend/controller/RestEndpoint.java
@@ -4,20 +4,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/endpoint")
 public class RestEndpoint {
 
+    private JdbcTemplate jdbcTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     @GetMapping(path = "/hello")
     public String hello() {
-        String response = "Hello man!";
-        return response;
+        return "Hello man!";
     }
 
     @GetMapping(path = "/getUrls")
-    public @ResponseBody Object getUrls() {
-        return null;
+    public @ResponseBody List<Map<String, Object>> getUrls() {
+        return this.jdbcTemplate.queryForList("select * from testschema.testtable");
     }
-    
+
 }
